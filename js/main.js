d6b808636c90ea3bb8c381c363853c58cf5e374e
jebusx33 <gabrielj82@gmail.com>
2020-10-10 00:29:01 -0300

    start portfolio

---
+++ js/main.js
@@ -0,0 +1,62 @@
+/* iniciando MUURI */
+const grid = new Muuri('.grid', {
+	layout: {
+		rounding: false
+	}
+});
+
+/*mostrar todas la imgenes juanta cuando esten la pagina cargada*/
+window.addEventListener('load', () => {
+    /*refresque le tamaño */
+    grid.refreshItems().layout();
+    /*muestra las imagenes */
+    document.getElementById('grid').classList.add('imagenes-cargadas');
+    /*enlaces de filtrado */
+    // Agregamos los listener de los enlaces para filtrar por categoria.
+    const enlaces = document.querySelectorAll('#categorias a');
+	enlaces.forEach((elemento) => {
+        //console.log(elemento);
+        elemento.addEventListener('click', (evento) => {
+			evento.preventDefault(); //eliminar simbolos
+            enlaces.forEach((enlace) => enlace.classList.remove('activo'));
+           
+            evento.target.classList.add('activo');
+           
+            const categoria = evento.target.innerHTML.toLowerCase();
+			categoria === 'todos' ? grid.filter('[data-categoria]') : grid.filter(`[data-categoria="${categoria}"]`);
+        });
+    });
+    
+
+	// Agregamos el listener para la barra de busqueda
+	document.querySelector('#barra-busqueda').addEventListener('input', (evento) => {
+		const busqueda = evento.target.value;
+		grid.filter( (item) => item.getElement().dataset.etiquetas.includes(busqueda) );
+	});
+
+	// Agregamos listener para las imagenes
+	const overlay = document.getElementById('overlay');
+	document.querySelectorAll('.grid .item img').forEach((elemento) => {
+		elemento.addEventListener('click', () => {
+			const ruta = elemento.getAttribute('src');
+			const descripcion = elemento.parentNode.parentNode.dataset.descripcion;
+
+			overlay.classList.add('activo');
+			document.querySelector('#overlay img').src = ruta;
+			document.querySelector('#overlay .descripcion').innerHTML = descripcion;
+		});
+	});
+
+	// Eventlistener del boton de cerrar
+	document.querySelector('#btn-cerrar-popup').addEventListener('click', () => {
+		overlay.classList.remove('activo');
+	});
+
+	// Eventlistener del overlay
+	overlay.addEventListener('click', (evento) => {
+		evento.target.id === 'overlay' ? overlay.classList.remove('activo') : '';
+	});
+
+
+
+});
